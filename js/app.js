ee1c660ca41cbbfdc641b6bf9436bac92f77fa73
Nkaemmerlen <nathankaemmerlen@yahoo.com>
2022-02-13 15:04:57 -0600

    style html and start css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /*--------------------------Constants-------------------------------*/
 import { allDecisions, allSituations, allEndings } from "../data/paths.js"
-console.log(allDecisions[7])
-console.log(allSituations[4])
-console.log(allEndings[2])
+// console.log(allDecisions[7])
+// console.log(allSituations[4])
+// console.log(allEndings[2])
 
 /*---------------------------Variables------------------------------*/
 
